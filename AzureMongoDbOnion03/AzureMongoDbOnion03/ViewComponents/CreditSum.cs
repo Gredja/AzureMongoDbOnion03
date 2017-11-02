52af83bffc30d55165e0e18f29fae1e7fd8590a9
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2017-11-02 13:07:08 +0300

    Ok

---
+++ AzureMongoDbOnion03/AzureMongoDbOnion03/ViewComponents/CreditSum.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AzureMongoDbOnion03.Domain.Services.DbServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureMongoDbOnion03.ViewComponents
+{
+    public class CreditSum : ViewComponent
+    {
+        private readonly IDbService _dbService;
+
+        public CreditSum(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        public async Task<string> Invoke()
+        {
+            var result = string.Empty;
+            var credits = await _dbService.GetAllCredits();
+
+            if (credits != null)
+            {
+                result = $"Вам должны: \n USD - {credits.Where(x => x.Currency == "USD").Sum(s => s.Amount)} \n" +
+                         $"EUR - {credits.Where(x => x.Currency == "EUR").Sum(s => s.Amount)} \n" +
+                         $"BYN - {credits.Where(x => x.Currency == "BYN").Sum(s => s.Amount)} \n";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AzureMongoDbOnion03/AzureMongoDbOnion03/ViewModels/MoneyPlusDebtorName.cs
@@ -0,0 +1,10 @@
+﻿using AzureMongoDbOnion03.Domain;
+
+namespace AzureMongoDbOnion03.ViewModels
+{
+    public class MoneyPlusDebtorName
+    {
+        public Credit Credit { get; set; }
+        public string DebtorName { get; set; }
+    }
+}
