7f023a7313dc36759b9e36a033e94dd0dc41432f
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2017-11-03 11:14:11 +0300

    Add repay controller

---
+++ AzureMongoDbOnion03/AzureMongoDbOnion03/Controllers/RepayController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using AzureMongoDbOnion03.Domain.Services.Services.DbServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureMongoDbOnion03.Controllers
+{
+    public class RepayController : Controller
+    {
+        private readonly IDbService _dbService;
+
+        public RepayController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
