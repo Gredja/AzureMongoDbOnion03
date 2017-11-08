ff9d68306c66bfc27f7e8774169a9d950249a649
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2017-11-08 18:28:29 +0300

    To be continued

--- AzureMongoDbOnion03/AzureMongoDbOnion03.Application.Services/Services/Auntification/Aunification.cs
+++ AzureMongoDbOnion03/AzureMongoDbOnion03.Application.Services/Services/Auntification/Aunification.cs
@@ -1,7 +1,9 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using AzureMongoDbOnion03.Application.Services.Model;
 using AzureMongoDbOnion03.Infrastructure.Data;
+using MongoDB.Driver;
 using Dto = AzureMongoDbOnion03.Infrastructure.Dto.Model;
 
 namespace AzureMongoDbOnion03.Application.Services.Services.Auntification
@@ -9,16 +11,36 @@ namespace AzureMongoDbOnion03.Application.Services.Services.Auntification
     public class Aunification : IAunification
     {
         private readonly IRepository<Dto.User> _userRepository;
+        private readonly IMapper _mapper;
 
-        public Aunification(IRepository<Dto.User> userRepository)
+        public Aunification(IRepository<Dto.User> userRepository, IMapper mapper)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
         }
 
-        public async Task<string> TryLogin(User user)
+        public async Task AddUser(User user)
+        {
+            var dtoUser = _mapper.Map<User, Dto.User>(user);
+            await _userRepository.AddOne(dtoUser);
+        }
+
+        public async Task<DeleteResult> DeleteUser(User user)
+        {
+            return await _userRepository.DeleteOne(user.Id);
+        }
+
+        public async Task<ReplaceOneResult> UpdateUser(User user)
+        {
+            var dtoUser = _mapper.Map<User, Dto.User>(user);
+            return await _userRepository.Update(dtoUser);
+        }
+
+        public async Task<User> TryLogin(User user)
         {
             var users = await _userRepository.GetAll();
-            return users.Where(x => x.Email == user.Email).FirstOrDefault(x => x.Password == user.Password).Name;
+            var regUser = users.Where(x => x.Email == user.Email).FirstOrDefault(x => x.Password == user.Password);
+            return _mapper.Map<Dto.User, User>(regUser);
         }
     }
 }
--- AzureMongoDbOnion03/AzureMongoDbOnion03.Application.Services/Services/Auntification/IAunification.cs
+++ AzureMongoDbOnion03/AzureMongoDbOnion03.Application.Services/Services/Auntification/IAunification.cs
@@ -1,10 +1,14 @@
 ﻿using System.Threading.Tasks;
 using AzureMongoDbOnion03.Application.Services.Model;
+using MongoDB.Driver;
 
 namespace AzureMongoDbOnion03.Application.Services.Services.Auntification
 {
     public interface IAunification
     {
-        Task<string> TryLogin(User user);
+        Task AddUser(User user);
+        Task<DeleteResult> DeleteUser(User user);
+        Task<ReplaceOneResult> UpdateUser(User user);
+        Task<User> TryLogin(User user);
     }
 }
